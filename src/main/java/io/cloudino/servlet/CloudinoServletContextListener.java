148c9bd756c042d0a97658c858c8705df965902f
SergioMartinezP <sergio.martinezp@infotec.com.mx>
2015-07-15 10:35:02 -0500

    Base para Docker

--- src/main/java/io/cloudino/servlet/CloudinoServletContextListener.java
+++ src/main/java/io/cloudino/servlet/CloudinoServletContextListener.java
@@ -5,12 +5,16 @@
  */
 package io.cloudino.servlet;
 
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 import org.semanticwb.datamanager.DataMgr;
+import org.semanticwb.datamanager.DataObject;
+import org.semanticwb.datamanager.SWBDataSource;
+import org.semanticwb.datamanager.SWBScriptEngine;
 
 @WebListener
 public class CloudinoServletContextListener implements ServletContextListener {
@@ -21,6 +25,16 @@ public class CloudinoServletContextListener implements ServletContextListener {
         log.info("Starting Cloudino Portal");
         System.out.println("aplicacion web arrancada");
         DataMgr.createInstance(sce.getServletContext().getRealPath("/"));
+        log.info("Cloudino Started");
+        SWBScriptEngine engine=DataMgr.getScriptEngine("/cloudino.js");
+        SWBDataSource ds=engine.getDataSource("Device");
+        try
+        {
+            DataObject obj=ds.fetch();
+            log.info(""+obj);
+        } catch (IOException ioe) {log.throwing("CloudinoServletContextListener","contextInitialized", ioe);}
+
+        engine.close();
     }
 
     @Override
--- src/main/resources/cloudino.js
+++ src/main/resources/cloudino.js
@@ -3,6 +3,8 @@ eng.dataStores["mongodb"]={
     host:"localhost",
     port:27017,
     class: "org.semanticwb.datamanager.datastore.DataStoreMongo",
+    envhost:"MONGO_PORT_27017_TCP_ADDR",
+    envport:"MONGO_PORT_27017_TCP_PORT",
 };
 
 //******* DataSorices ************
