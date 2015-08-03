c63758955f94d3cbd4f0840dfcec449bfb94afe9
SergioMartinezP <sergio.martinezp@infotec.com.mx>
2015-08-03 12:16:57 -0500

    Cambios al router
    Se agregó búsqueda para la ruta Raiz del contexto y para proteger rutas sin handler

--- src/main/java/io/cloudino/servlet/router/Router.java
+++ src/main/java/io/cloudino/servlet/router/Router.java
@@ -19,17 +19,16 @@ import org.semanticwb.datamanager.script.ScriptObject;
 public class Router {
     private static final Logger logger = Logger.getLogger("i.c.s.r.Router");
     private static Router instance = null;
-    private final Map<String, RouteHandler> absoluteRoutes  = new ConcurrentHashMap<>();
-    private final Map<String, RouteHandler> relativeRoutes = new ConcurrentHashMap<>();
+    private final Map<String, RouteHandler> routes  = new ConcurrentHashMap<>();
     private final Set<String> securedRoutes = new TreeSet<>();
     private final String loginRoute;
     private final MustacheFactory mf = new DefaultMustacheFactory();
     
     private Router(ScriptObject config){
         loginRoute = (null==config.getString("loginFallback")?"login":config.getString("loginFallback"));
-        ScriptObject routes = config.get("routeList");
-        if (routes.isArray()){
-            routes.values().forEach(this::process);
+        ScriptObject routesFile = config.get("routeList");
+        if (routesFile.isArray()){
+            routesFile.values().forEach(this::process);
         }
     }
     
@@ -41,41 +40,33 @@ public class Router {
         instance = new Router(config);
     }
     private void process(ScriptObject path){
-        try {System.out.println("->"+path);
+        try {
+            if ("true".equalsIgnoreCase(path.getString("isRestricted"))){
+                securedRoutes.add(path.getString("routePath"));
+            }
+            if (null==path.getString("routeHandler")) return;
             RouteHandler rh = (RouteHandler)Class.forName(path.getString("routeHandler")).newInstance();
             String template = path.getString("template");
             InputStreamReader reader = new InputStreamReader(LoginHandler.class.getResourceAsStream("/templates/"+template+".mustache"));
             Mustache mustache = mf.compile(reader, template+".mustache");
             rh.config(mustache);
-            if ("true".equalsIgnoreCase(path.getString("isTree"))){
-                relativeRoutes.put(path.getString("routePath"), rh);
-            } else {
-                absoluteRoutes.put(path.getString("routePath"), rh);
-            }
-            if ("true".equalsIgnoreCase(path.getString("isRestricted"))){
-                securedRoutes.add(path.getString("routePath"));
-            }
+            routes.put(path.getString("routePath"), rh);
         } catch (ReflectiveOperationException cnf) {
             logger.severe("****** Can't load class: "+path.getString("routeHandler"));
             Runtime.getRuntime().exit(10);
         }
     }
     private RouteHandler getRouterHandler(final String path, final DataObject user){
-        System.out.println("route: "+path);
         String route = path.substring(1);
         if (route.contains("/")){
             route=route.substring(0, route.indexOf("/"));
         }
-        logger.fine("processing route:"+route); System.out.println("parsed route: "+route);
+        logger.fine("processing route: "+route); 
         if(securedRoutes.contains(route) && 
                 ((null==user) || 
                 !"true".equalsIgnoreCase(user.getString("isSigned")))){ System.out.println("is Secure, but no User, going for loginRoute");
-            return absoluteRoutes.get(loginRoute);
+            return routes.get(loginRoute);
         }
-        if(relativeRoutes.containsKey(route)){
-            return relativeRoutes.get(route);
-        } else {
-            return absoluteRoutes.get(route);
-        }
+        return routes.get(route);
     } 
 }
