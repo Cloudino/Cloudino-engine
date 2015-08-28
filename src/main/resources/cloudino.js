f547da7355adcf428e854af185380d246bd23ff4
softjei <softjei@gmail.com>
2015-08-28 16:22:26 -0500

    Desarrollo Cloudino Varios

--- src/main/resources/cloudino.js
+++ src/main/resources/cloudino.js
@@ -1,7 +1,7 @@
 //******* DataStores ***************
 
 eng.config={
-    devicePort:9595,
+    devicePort:8080,
     arduinoPath:"/Applications/Arduino.app/Contents/Java",
     usersWorkPath:"/cloudino/users",
 };
