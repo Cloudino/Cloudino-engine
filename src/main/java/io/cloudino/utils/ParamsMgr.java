3be04510a323d92370bd9ed32be377feb14e4ea1
softjei <softjei@gmail.com>
2015-09-08 18:57:22 -0500

    Desarrollo Cloudino Varios

---
+++ src/main/java/io/cloudino/utils/ParamsMgr.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package io.cloudino.utils;
+
+import java.util.HashMap;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author javiersolis
+ */
+public class ParamsMgr 
+{
+    private static final String SESSION_TAG="_PARAMSMGR_";
+    private final HttpSession session;
+    private HashMap<String,HashMap<String,String>> params;
+
+    public ParamsMgr(HttpSession session) {
+        this.session=session;
+        params=(HashMap)session.getAttribute(SESSION_TAG);
+        if(params==null)
+        {
+            params=new HashMap();
+            session.setAttribute(SESSION_TAG, params);
+        }
+    }
+    
+    public String setDataMap(HashMap<String,String> map)
+    {
+        String key=TokenGenerator.nextToken();
+        params.put(key, map);
+        return key;
+    }
+    
+    /**
+     * Parametros llave,valor
+     * @param data
+     * @return 
+     */
+    public String setDataValues(String ...data)
+    {
+        String key=TokenGenerator.nextToken();
+        HashMap map=new HashMap();
+        if(data.length%2==0)
+        {
+            for(int x=0;x<data.length;x+=2)
+            {
+                map.put(data[x], data[x+1]);
+            }
+            params.put(key, map);
+        }else
+        {
+            return null;
+        }
+        return key;
+    }
+    
+    public String setDataValue(String value)
+    {
+        String key=TokenGenerator.nextToken();
+        HashMap<String,String> map=new HashMap();
+        map.put("_DEF_", value);
+        params.put(key, map);
+        return key;
+    }    
+    
+    public HashMap<String,String> getDataMap(String key)
+    {
+        return params.get(key);
+    }
+    
+    public String getDataValue(String key)
+    {
+        if(params.containsKey(key))
+        {
+            return params.get(key).get("_DEF_");
+        }
+        return null;        
+    }    
+    
+    public String getDataValue(String key, String param)
+    {
+        if(params.containsKey(key))
+        {
+            return params.get(key).get(param);
+        }
+        return null;
+    }
+}
---
+++ src/main/java/io/cloudino/utils/Utils.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package io.cloudino.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ *
+ * @author javiersolis
+ */
+public class Utils {
+    private static final int BUFFERSIZE=8192;
+    
+        /**
+         * Copies an input stream into an output stream using the buffer size
+         * defined by {@code SWBUtils.bufferSize} in the reading/writing
+         * operations.
+         * <p>
+         * Copia un flujo de entrada en uno de salida utilizando el
+         * tama&ntilde;o de buffer definido por {@code SWBUtils.bufferSize} en
+         * las operaciones de lectura/escritura.</p>
+         *
+         * @param in the input stream to read from
+         * @param out the output stream to write to
+         * @throws IOException if either the input or the output stream is
+         * {@code null}.
+         * <p>
+         * Si el flujo de entrada o el de salida es {@code null}.</p>
+         */
+        public static void copyStream(InputStream in, OutputStream out) throws IOException
+        {
+            copyStream(in, out, BUFFERSIZE);
+        }
+
+        /**
+         * Copies an input stream into an output stream using the specified
+         * buffer size in the reading/writing operations.
+         * <p>
+         * Copia un flujo de entrada en uno de salida utilizando el
+         * tama&ntilde;o de buffer especificado en las operaciones de
+         * lectura/escritura.</p>
+         *
+         * @param in the input stream to read from
+         * @param out the output stream to write to
+         * @param bufferSize the number of bytes read/writen at the same time in
+         * each I/O operation
+         * @throws IOException if either the input or the output stream is
+         * {@code null}.
+         * <p>
+         * Si el flujo de entrada o el de salida es {@code null}.</p>
+         */
+        public static void copyStream(InputStream in, OutputStream out, int bufferSize) throws IOException
+        {
+            if (in == null)
+            {
+                throw new IOException("Input Stream null");
+            }
+            if (out == null)
+            {
+                throw new IOException("Ouput Stream null");
+            }
+            byte[] bfile = new byte[bufferSize];
+            int x;
+            while ((x = in.read(bfile, 0, bufferSize)) > -1)
+            {
+                out.write(bfile, 0, x);
+            }
+            in.close();
+            out.flush();
+            out.close();
+        }
+}
