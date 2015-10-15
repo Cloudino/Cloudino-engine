7151d69551b8874ea990117d68af6ec407864ac2
SergioMartinezP <sergio.martinezp@infotec.mx>
2015-10-15 17:33:02 -0500

    ScriptEngine interaction to SWBDataManager

--- src/main/java/JavaScriptTest.java
+++ src/main/java/JavaScriptTest.java
@@ -1,11 +1,15 @@
 
+import io.cloudino.rules.scriptengine.RuleEngineProvider;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Map;
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import jdk.nashorn.api.scripting.ScriptObjectMirror;
+import org.semanticwb.datamanager.DataMgr;
+import org.semanticwb.datamanager.SWBScriptEngine;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -17,13 +21,33 @@ import jdk.nashorn.api.scripting.ScriptObjectMirror;
  * @author javiersolis
  */
 public class JavaScriptTest {
-    
-    public static String serialize(Object obj)
-    {
-        StringBuilder ret=new StringBuilder();
-        if(obj instanceof ScriptObjectMirror)
-        {
-            ScriptObjectMirror om=(ScriptObjectMirror)obj; 
+
+    public static void main(String[] args) throws ScriptException {
+        DataMgr.createInstance(null);
+        RuleEngineProvider rep = new RuleEngineProvider(10);
+        ScriptEngine engine = rep.getEngine("_suri:Cloudino:CloudRule:1");
+        engine = rep.getEngine("_suri:Cloudino:CloudRule:2");
+        engine = rep.getEngine("_suri:Cloudino:CloudRule:3");
+        engine = rep.getEngine("_suri:Cloudino:CloudRule:4");
+        engine = rep.getEngine("_suri:Cloudino:CloudRule:5");
+        engine = rep.getEngine("_suri:Cloudino:CloudRule:6");
+        ScriptObjectMirror bind = ((ScriptObjectMirror) engine.getBindings(ScriptContext.ENGINE_SCOPE));
+        ScriptEngine[] arr1 = useLotsOfMemory();
+        System.gc();
+        ((ScriptObjectMirror) ((ScriptObjectMirror) bind.get("events")).getSlot(0)).callMember("funct", "hola");
+        arr1 = useLotsOfMemory();
+        arr1[500].eval("print('memorias....')");
+        arr1=null;
+        System.gc();
+        engine = rep.getEngine("_suri:Cloudino:CloudRule:1");
+        bind = ((ScriptObjectMirror) engine.getBindings(ScriptContext.ENGINE_SCOPE));
+        ((ScriptObjectMirror) ((ScriptObjectMirror) bind.get("events")).getSlot(0)).callMember("funct", "hola");
+    }
+
+    public static String serialize(Object obj) {
+        StringBuilder ret = new StringBuilder();
+        if (obj instanceof ScriptObjectMirror) {
+            ScriptObjectMirror om = (ScriptObjectMirror) obj;
 //            System.out.println(om+" isArray "+om.isArray());
 //            System.out.println(om+" isEmpty "+om.isEmpty());
 //            System.out.println(om+" isExtensible "+om.isExtensible());
@@ -32,71 +56,66 @@ public class JavaScriptTest {
 //            System.out.println(om+" isSealed "+om.isSealed());
 //            System.out.println(om+" isStrictFunction "+om.isStrictFunction());            
 //            System.out.println(om+" getOwnKeys "+Arrays.asList(om.getOwnKeys(true)));  
-            
-            if(om.isFunction())
-            {
+
+            if (om.isFunction()) {
                 ret.append(om.toString());
-            }else if(om.isArray())
-            {
+            } else if (om.isArray()) {
                 ret.append("[");
                 //ret.append("isArray:"+om.toString());
-                for(int x=0;x<om.size();x++)
-                {
-                    Object o=om.getSlot(x);
+                for (int x = 0; x < om.size(); x++) {
+                    Object o = om.getSlot(x);
                     ret.append(serialize(o));
-                    if(x+1<om.size())ret.append(",");
+                    if (x + 1 < om.size()) {
+                        ret.append(",");
+                    }
                 }
                 ret.append("]");
-            }else if(om.toString().indexOf("global")>-1)
-            {
-                Iterator<Map.Entry<String,Object>> it=om.entrySet().iterator();
+            } else if (om.toString().indexOf("global") > -1) {
+                Iterator<Map.Entry<String, Object>> it = om.entrySet().iterator();
                 while (it.hasNext()) {
-                   Map.Entry<String, Object> entry = it.next();
-                   ret.append("var "+entry.getKey()+"="+serialize(entry.getValue())+";\n");
-               }
-            }else
-            {
+                    Map.Entry<String, Object> entry = it.next();
+                    ret.append("var " + entry.getKey() + "=" + serialize(entry.getValue()) + ";\n");
+                }
+            } else {
                 ret.append("{");
-                Iterator<Map.Entry<String,Object>> it=om.entrySet().iterator();
+                Iterator<Map.Entry<String, Object>> it = om.entrySet().iterator();
                 while (it.hasNext()) {
-                   Map.Entry<String, Object> entry = it.next();
-                   ret.append(entry.getKey()+":"+serialize(entry.getValue()));
-                   if(it.hasNext())ret.append(",");
-               }
-               ret.append("}");
+                    Map.Entry<String, Object> entry = it.next();
+                    ret.append(entry.getKey() + ":" + serialize(entry.getValue()));
+                    if (it.hasNext()) {
+                        ret.append(",");
+                    }
+                }
+                ret.append("}");
             }
-        }else if(obj instanceof String)
-        {
-            ret.append("\""+obj+"\"");
-        }else
-        {
+        } else if (obj instanceof String) {
+            ret.append("\"" + obj + "\"");
+        } else {
             ret.append(obj);
         }
         return ret.toString();
     }
-    
-    public static void main(String[] args) throws Exception
-    {
+
+    public static void main3(String[] args) throws Exception {
         ScriptEngineManager factory = new ScriptEngineManager();
         ScriptEngine engine = factory.getEngineByName("JavaScript");
         //engine.eval("function p(x){print(x);};var c=function(x){print(x);};var i=function(){x++;};var x=10;var cap=function(z){return function(msg){print(msg+\" \"+z);}}(x);");
-        String script="" +
-"var events=[];\n" +
-"var msg;\n" +
-"{ \n" +
-"	var _cntx=\"contx1\"; \n" +
-"	events.push({cntx:_cntx,type:\"cdino_ondevice_message\",funct:function(msg){print(msg);},params:{topic:\"topic\"}});\n" +
-"}";    
+        String script = ""
+                + "var events=[];\n"
+                + "var msg;\n"
+                + "{ \n"
+                + "	var _cntx=\"contx1\"; \n"
+                + "	events.push({cntx:_cntx,type:\"cdino_ondevice_message\",funct:function(msg){print(msg);},params:{topic:\"topic\"}});\n"
+                + "}";
         engine.eval(script);
-        ScriptObjectMirror bind=((ScriptObjectMirror)engine.getBindings(ScriptContext.ENGINE_SCOPE));
+        ScriptObjectMirror bind = ((ScriptObjectMirror) engine.getBindings(ScriptContext.ENGINE_SCOPE));
         //bind.callMember("i");
         //bind.callMember("cap","hola");
-        
-        String txt=serialize(bind);
+
+        String txt = serialize(bind);
         System.out.println(txt);
-        
+
         //System.out.println(xml);
-        
 //        long time=System.currentTimeMillis();
 //        for(int x=0;x<1000;x++)
 //        {
@@ -110,47 +129,50 @@ public class JavaScriptTest {
 //            txt=serialize(bind);
 //        }
 //        System.out.println("Time:"+(System.currentTimeMillis()-time));
-        
-        
-        
         engine = factory.getEngineByName("JavaScript");
         engine.eval(txt);
 
-        bind=((ScriptObjectMirror)engine.getBindings(ScriptContext.ENGINE_SCOPE));
-        
-        ((ScriptObjectMirror)((ScriptObjectMirror)bind.get("events")).getSlot(0)).callMember("funct", "hola");
-        
+        bind = ((ScriptObjectMirror) engine.getBindings(ScriptContext.ENGINE_SCOPE));
+
+        ((ScriptObjectMirror) ((ScriptObjectMirror) bind.get("events")).getSlot(0)).callMember("funct", "hola");
+
         //bind.callMember("i");
         //bind.callMember("cap","hola");
-        
         //System.out.println(engine.getFactory().getParameter("THREADING"));
-        
-        txt=serialize(bind);
-        System.out.println(txt);        
+        txt = serialize(bind);
+        System.out.println(txt);
     }
 
     public static void main2(String[] args) throws Exception {
         ScriptEngineManager factory = new ScriptEngineManager();
         ScriptEngine engine = factory.getEngineByName("JavaScript");
 
-        System.out.println(((ScriptObjectMirror)engine.getBindings(ScriptContext.ENGINE_SCOPE)).size());
-        System.out.println("x:"+engine.get("x"));
-        System.out.println("var x:"+engine.eval("function p(x){print(x);};x=10;"));
-        System.out.println("x:"+engine.get("x"));
-        System.out.println(((ScriptObjectMirror)engine.getBindings(ScriptContext.ENGINE_SCOPE)).size());
-        
-        ScriptObjectMirror bind=((ScriptObjectMirror)engine.getBindings(ScriptContext.ENGINE_SCOPE));
+        System.out.println(((ScriptObjectMirror) engine.getBindings(ScriptContext.ENGINE_SCOPE)).size());
+        System.out.println("x:" + engine.get("x"));
+        System.out.println("var x:" + engine.eval("function p(x){print(x);};x=10;"));
+        System.out.println("x:" + engine.get("x"));
+        System.out.println(((ScriptObjectMirror) engine.getBindings(ScriptContext.ENGINE_SCOPE)).size());
+
+        ScriptObjectMirror bind = ((ScriptObjectMirror) engine.getBindings(ScriptContext.ENGINE_SCOPE));
         bind.callMember("p", "Hola");
-        
+
         engine = factory.getEngineByName("JavaScript");
         //engine.setBindings(engine.createBindings(),ScriptContext.ENGINE_SCOPE);
-        
-        System.out.println(((ScriptObjectMirror)engine.getBindings(ScriptContext.ENGINE_SCOPE)).size());
-        System.out.println("x:"+engine.get("x"));
-        System.out.println("var x:"+engine.eval("function p(x){print(x);};x=10;"));
-        System.out.println("x:"+engine.get("x"));
-        System.out.println(((ScriptObjectMirror)engine.getBindings(ScriptContext.ENGINE_SCOPE)).size());
-        
 
+        System.out.println(((ScriptObjectMirror) engine.getBindings(ScriptContext.ENGINE_SCOPE)).size());
+        System.out.println("x:" + engine.get("x"));
+        System.out.println("var x:" + engine.eval("function p(x){print(x);};x=10;"));
+        System.out.println("x:" + engine.get("x"));
+        System.out.println(((ScriptObjectMirror) engine.getBindings(ScriptContext.ENGINE_SCOPE)).size());
+
+    }
+
+    private static ScriptEngine[] useLotsOfMemory() {
+        ScriptEngineManager factory = new ScriptEngineManager();
+        ScriptEngine engines[] = new ScriptEngine[10000];
+        for (int i = 0; i < 10000; i++) {
+            engines[i] = factory.getEngineByName("nashorn");
+        }
+        return engines;
     }
 }
--- src/main/java/io/cloudino/rules/scriptengine/RuleEngineProvider.java
+++ src/main/java/io/cloudino/rules/scriptengine/RuleEngineProvider.java
@@ -3,14 +3,17 @@ package io.cloudino.rules.scriptengine;
 import java.io.IOException;
 import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptException;
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
 import org.semanticwb.datamanager.DataMgr;
 import org.semanticwb.datamanager.DataObject;
 import org.semanticwb.datamanager.SWBDataSource;
@@ -23,7 +26,7 @@ import org.semanticwb.datamanager.SWBScriptEngine;
 public class RuleEngineProvider {
 
     private static final Logger logger = Logger.getLogger("i.c.r.se.RuleEngineProvider");
-    private final Map<String, SoftReference> hash = new ConcurrentHashMap<String, SoftReference>();
+    private final Map<String, SoftReference> hash = new ConcurrentHashMap<>();
     private final int HARD_SIZE;
     private final LinkedList hardCache = new LinkedList();
     private final ReferenceQueue queue = new ReferenceQueue();
@@ -73,9 +76,9 @@ public class RuleEngineProvider {
         }
     }
 
-    private ScriptEngine put(String key, ScriptEngine value) {
+    private SoftValue put(String key, ScriptEngine value) {
         processQueue();
-        return (ScriptEngine) hash.put(key, new SoftValue(value, key, queue)).get();
+        return (SoftValue) hash.put(key, new SoftValue(value, key, queue));
     }
 
     private ScriptEngine remove(Object key) {
@@ -85,27 +88,111 @@ public class RuleEngineProvider {
     }
 
     private ScriptEngine retreiveFromSerialized(String id) {
-        return null; //TODO: implementa retreive serialized bindings
-    }
-
-    private ScriptEngine create(String id) {
-        ScriptEngine result;
+        ScriptEngine result = null;
         try {
-            SWBScriptEngine engine = DataMgr.getUserScriptEngine("/cloudino.js", null);
-            SWBDataSource ds = engine.getDataSource("CloudRule");
-            DataObject cloudRule = ds.fetchObjById(id);
-            String script = cloudRule.getString("script");
-            result = NashornEngineFactory.getEngine(300, TimeUnit.MILLISECONDS);
-            result.eval(script);
+            String state = getStringFromCloudRule(id, "state");
+            if (null != state) {
+                result = NashornEngineFactory.getEngine(300, TimeUnit.MILLISECONDS);
+                result.eval(state);
+            }
         } catch (InterruptedException | IOException | ScriptException exp) {
-            logger.log(Level.WARNING, "can't create script with id: "+ id, exp);
+            logger.log(Level.WARNING, "can't create script with id: " + id, exp);
             result = null;
         }
         return result;
     }
 
-    private void serializeAndSave(String  key, ScriptEngine scriptEngine) {
-        //TODO: serialize bindings and save to some storage
+    private ScriptEngine create(String id) {
+        ScriptEngine result = null;
+        try {
+            String script = getStringFromCloudRule(id, "script");
+            if (null != script) {
+                result = NashornEngineFactory.getEngine(300, TimeUnit.MILLISECONDS);
+                result.eval(script);
+            }
+        } catch (InterruptedException | IOException | ScriptException exp) {
+            logger.log(Level.WARNING, "can't create script with id: " + id, exp);
+            result = null;
+        }
+        return result;
+    }
+
+    private String getStringFromCloudRule(String id, String field) throws IOException {
+        SWBScriptEngine engine = DataMgr.getUserScriptEngine("/cloudino.js", null);
+        SWBDataSource ds = engine.getDataSource("CloudRule");
+        DataObject cloudRule = ds.fetchObjById(id);
+        return cloudRule.getString(field);
+    }
+
+    private void serializeAndSave(String id, ScriptEngine scriptEngine) {
+        try { System.out.println("Serializing....");
+            SWBScriptEngine engine = DataMgr.getUserScriptEngine("/cloudino.js", null);
+            SWBDataSource ds = engine.getDataSource("CloudRule");
+            DataObject cloudRule = ds.fetchObjById(id);
+            cloudRule.put("state", serialize(scriptEngine.getBindings(ScriptContext.ENGINE_SCOPE)));
+            ds.updateObj(cloudRule);
+        } catch (IOException ioe) {
+        }
+    }
+
+    private String serialize(Object obj) {
+        StringBuilder ret = new StringBuilder();
+        if (obj instanceof ScriptObjectMirror) {
+            ScriptObjectMirror om = (ScriptObjectMirror) obj;
+//            System.out.println(om+" isArray "+om.isArray());
+//            System.out.println(om+" isEmpty "+om.isEmpty());
+//            System.out.println(om+" isExtensible "+om.isExtensible());
+//            System.out.println(om+" isFrozen "+om.isFrozen());
+//            System.out.println(om+" isFunction "+om.isFunction());
+//            System.out.println(om+" isSealed "+om.isSealed());
+//            System.out.println(om+" isStrictFunction "+om.isStrictFunction());            
+//            System.out.println(om+" getOwnKeys "+Arrays.asList(om.getOwnKeys(true)));  
+
+            if (om.isFunction()) {
+                ret.append(om.toString());
+            } else if (om.isArray()) {
+                ret.append("[");
+                //ret.append("isArray:"+om.toString());
+                for (int x = 0; x < om.size(); x++) {
+                    Object o = om.getSlot(x);
+                    ret.append(serialize(o));
+                    if (x + 1 < om.size()) {
+                        ret.append(",");
+                    }
+                }
+                ret.append("]");
+            } else if (om.toString().contains("global")) {
+                Iterator<Map.Entry<String, Object>> it = om.entrySet().iterator();
+                while (it.hasNext()) {
+                    Map.Entry<String, Object> entry = it.next();
+                    ret.append("var ")
+                            .append(entry.getKey())
+                            .append("=")
+                            .append(serialize(entry.getValue()))
+                            .append(";\n");
+                }
+            } else {
+                ret.append("{");
+                Iterator<Map.Entry<String, Object>> it = om.entrySet().iterator();
+                while (it.hasNext()) {
+                    Map.Entry<String, Object> entry = it.next();
+                    ret.append(entry.getKey())
+                            .append(":")
+                            .append(serialize(entry.getValue()));
+                    if (it.hasNext()) {
+                        ret.append(",");
+                    }
+                }
+                ret.append("}");
+            }
+        } else if (obj instanceof String) {
+            ret.append("\"")
+                    .append(obj)
+                    .append("\"");
+        } else {
+            ret.append(obj);
+        }
+        return ret.toString();
     }
 
     private static class SoftValue extends SoftReference {
